35173be1e8ef09a6298790178a58485f1a273e58
Benny Freshness <bkoonse@gmail.com>
2014-10-13 18:35:34 -0700

    friendly w/ rails 3

--- lib/make_flaggable/flagging.rb
+++ lib/make_flaggable/flagging.rb
@@ -2,5 +2,7 @@ module MakeFlaggable
   class Flagging < ActiveRecord::Base
     belongs_to :flaggable, :polymorphic => true, counter_cache: true
     belongs_to :flagger, :polymorphic => true, counter_cache: true
+
+    attr_accessible :flaggable, :flagger, :reason
   end
 end
