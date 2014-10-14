77ee43953d2eff204b06f9d1b2b5947724e9fe10
Benny Freshness <bkoonse@gmail.com>
2014-10-13 18:56:22 -0700

    k changing back to reason and removing counter caches

--- lib/make_flaggable/flagging.rb
+++ lib/make_flaggable/flagging.rb
@@ -1,7 +1,7 @@
 module MakeFlaggable
   class Flagging < ActiveRecord::Base
-    belongs_to :flaggable, :polymorphic => true, counter_cache: true
-    belongs_to :flagger, :polymorphic => true, counter_cache: true
+    belongs_to :flaggable, :polymorphic => true
+    belongs_to :flagger, :polymorphic => true
 
     attr_accessible :flaggable, :flagger, :flag
   end
